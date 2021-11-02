d8f65405d2ce9e18977c87cdfccd52a5f3d15de7
SarathArrepu <a.sarath2090737@gmail.com>
2021-11-01 22:29:10 -0500

    added fan

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fanRouter = require('./routes/fan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/fan', fanRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
